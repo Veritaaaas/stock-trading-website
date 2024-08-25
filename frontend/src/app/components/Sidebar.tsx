01aecef6660227aa46799dc97e6fec455d6649f7
Veritaaaas <rodneyjamesmmaisog@iskolarngbayan.pup.edu.ph>
2024-08-25 21:03:00 +0800

    finish the sidebar component

---
+++ frontend/src/app/components/Sidebar.tsx
@@ -0,0 +1,47 @@
+import Image from "next/image";
+import { FaEye } from "react-icons/fa";
+import { RiHome6Line } from "react-icons/ri";
+import { RxDashboard } from "react-icons/rx";
+import { IoWalletOutline } from "react-icons/io5";
+import { FaRegBookmark } from "react-icons/fa6";
+import { HiOutlineNewspaper } from "react-icons/hi2";
+
+export default function Sidebar() {
+    return (
+        <div className="bg-white w-[300px] h-screen p-8 flex flex-col gap-8">
+            <div className="flex justify-center gap-3">
+                <Image src="Subtract.svg" alt="Subtract" width={35} height={35} />
+                <h1 className="text-2xl font-bold text-[#1C1C1C]">GoStock</h1>
+            </div>
+            <div className="bg-[#1c1c1c] rounded-xl p-4 flex flex-col gap-2">
+                <h1 className="text-lg text-white">Buying Power</h1>
+                <div className="flex gap-2 items-center">
+                    <h1 className="text-xl text-white font-bold">$0.00</h1>
+                    <FaEye className="text-white" />
+                </div>
+            </div>
+            <div className="flex flex-col text-[20px] font-bold gap-5">
+                <div className="flex gap-3 items-center">
+                    <RiHome6Line />
+                    <h1>Home</h1>
+                </div>
+                <div className="flex gap-3 items-center">
+                    <RxDashboard />
+                    <h1>Dashboard</h1>
+                </div>
+                <div className="flex gap-3 items-center">
+                    <IoWalletOutline />
+                    <h1>Wallet</h1>
+                </div>
+                <div className="flex gap-3 items-center">
+                    <FaRegBookmark />
+                    <h1>Watchlist</h1>
+                </div>
+                <div className="flex gap-3 items-center">
+                    <HiOutlineNewspaper />
+                    <h1>News</h1>
+                </div>
+            </div>
+        </div>
+    );
+}
--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -4,10 +4,13 @@ import { auth } from "./firebase/config.js";
 import { useRouter } from "next/navigation";
 import { signOut } from "firebase/auth";
 import { useEffect } from "react";
+import  Header  from "./components/Header.tsx";
+import Sidebar from "./components/Sidebar.tsx";
 
 export default function Home() {
   const [user] = useAuthState(auth);
   const router = useRouter();
+
 
   // Redirect to login page if user is not logged in
   useEffect(() => {
@@ -31,11 +34,9 @@ export default function Home() {
   };
 
   return (
-    <main className="font-sans">
-      <h2>Index</h2>
-      <button onClick={handleLogout} className="bg-red-500 text-white p-2 rounded">
-        Logout
-      </button>
+    <main className="font-sans bg-[#F5F7F9] h-screen">
+      <Sidebar />
+      <Header />
     </main>
   );
 }
