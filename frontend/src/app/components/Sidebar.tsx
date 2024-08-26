f4bdd69816d00f00a9f93695aea6ec15b12d3544
Veritaaaas <rodneyjamesmmaisog@iskolarngbayan.pup.edu.ph>
2024-08-26 12:56:41 +0800

    Update Sidebar component to dynamically render sidebar items

--- frontend/src/app/components/Sidebar.tsx
+++ frontend/src/app/components/Sidebar.tsx
@@ -5,8 +5,40 @@ import { RxDashboard } from "react-icons/rx";
 import { IoWalletOutline } from "react-icons/io5";
 import { FaRegBookmark } from "react-icons/fa6";
 import { HiOutlineNewspaper } from "react-icons/hi2";
+import { usePathname } from "next/navigation";
 
 export default function Sidebar() {
+
+    const pathname = usePathname();
+
+    const sidebarItems = [
+        {
+            name: "Home",
+            icon: <RiHome6Line />,
+            path: "/"
+        },
+        {
+            name: "Dashboard",
+            icon: <RxDashboard />,
+            path: "/dashboard"
+        },
+        {
+            name: "Wallet",
+            icon: <IoWalletOutline />,
+            path: "/wallet"
+        },
+        {
+            name: "Watchlist",
+            icon: <FaRegBookmark />,
+            path: "/watchlist"
+        },
+        {
+            name: "News",
+            icon: <HiOutlineNewspaper />,
+            path: "/news"
+        },
+    ];
+
     return (
         <div className="bg-white w-full h-screen p-8 flex flex-col gap-8">
             <div className="flex justify-center gap-3">
@@ -21,26 +53,12 @@ export default function Sidebar() {
                 </div>
             </div>
             <div className="flex flex-col text-[20px] font-bold gap-5">
-                <div className="flex gap-3 items-center">
-                    <RiHome6Line />
-                    <h1>Home</h1>
-                </div>
-                <div className="flex gap-3 items-center">
-                    <RxDashboard />
-                    <h1>Dashboard</h1>
-                </div>
-                <div className="flex gap-3 items-center">
-                    <IoWalletOutline />
-                    <h1>Wallet</h1>
-                </div>
-                <div className="flex gap-3 items-center">
-                    <FaRegBookmark />
-                    <h1>Watchlist</h1>
-                </div>
-                <div className="flex gap-3 items-center">
-                    <HiOutlineNewspaper />
-                    <h1>News</h1>
-                </div>
+                {sidebarItems.map((item, index) => (
+                    <div key={index} className={`flex gap-3 items-center px-2 cursor-pointer hover:bg-[#d9d9d9] rounded-lg ${pathname === item.path ? "bg-[#1c1c1c] text-white" : ""}`}>
+                        {item.icon}
+                        <h1>{item.name}</h1>
+                    </div>
+                ))}
             </div>
         </div>
     );
