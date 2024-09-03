c38edaa1dea544f41fa931f46b2752241e3b2a69
Veritaaaas <rodneyjamesmmaisog@iskolarngbayan.pup.edu.ph>
2024-09-03 16:23:59 +0800

    Add UserProvider component for managing user data

---
+++ frontend/src/components/UserProvider.tsx
@@ -0,0 +1,58 @@
+'use client'
+import { db } from "../firebase/config";
+import { collection, getDocs, query, where } from 'firebase/firestore';
+import { useAuthState } from "react-firebase-hooks/auth";
+import { auth } from "../firebase/config"; 
+import { createContext, useContext, useEffect, useState } from 'react';
+
+interface UserData {
+    uid: string;
+    username: string;
+    cash: number;
+    firstName: string;
+    lastName: string;
+}
+
+const UserContext = createContext<UserData | null>(null);
+
+export const UserProvider = ({ children }: { children: React.ReactNode }) => {
+    const [user] = useAuthState(auth);
+    const [userData, setUserData] = useState<UserData | null>(null);
+
+    useEffect(() => {
+        const fetchUserData = async () => {
+            if (user) {
+                const usersCollectionRef = collection(db, 'users');
+                const q = query(usersCollectionRef, where('uid', '==', user.uid)); 
+
+                try {
+                    const querySnapshot = await getDocs(q);
+                    if (!querySnapshot.empty) {
+                        const userDoc = querySnapshot.docs[0];
+                        setUserData(userDoc.data() as UserData);
+                    } else {
+                        console.log('No user document found');
+                    }
+                } catch (error) {
+                    console.error('Error fetching user data:', error);
+                }
+            }
+        };
+
+        fetchUserData();
+    }, [user]); 
+
+    return (
+        <UserContext.Provider value={userData}>
+            {children}
+        </UserContext.Provider>
+    );
+}
+
+export const useUser = () => {
+    const context = useContext(UserContext);
+    if (context === undefined) {
+        throw new Error('useUser must be used within a UserProvider');
+    }
+    return context;
+}
