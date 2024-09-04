9c739d00a5213177851bebaf10dc8cff3a92971c
Veritaaaas <rodneyjamesmmaisog@iskolarngbayan.pup.edu.ph>
2024-09-04 10:01:43 +0800

    Update MarketOverview component with responsive height and width

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -7,25 +7,24 @@ import dynamic from "next/dynamic";
 import { BiErrorAlt } from "react-icons/bi";
 import LinearProgress from '@mui/material/LinearProgress';
 
+const MarketOverviewNoSSR = dynamic(
+  () => import('react-ts-tradingview-widgets').then((mod) => mod.MarketOverview),
+  {
+    ssr: false
+  }
+);
 
 export default function Home() {
   const [user, loading] = useAuthState(auth);
   const router = useRouter();
 
-  // Redirect to login page if user is not logged in
   useEffect(() => {
+    console.log(user);
     if (!loading && !user) {
       router.push("/login");
     }
   }, [user, loading, router]);
 
-
-  const MarketOverviewNoSSR = dynamic(
-    () => import('react-ts-tradingview-widgets').then((mod) => mod.MarketOverview),
-    {
-      ssr: false
-    }
-  )
 
   if (loading) {
     return <LinearProgress />;
@@ -42,20 +41,20 @@ export default function Home() {
           </div>
         </div>
       </div>
-      <div className="grid grid-cols-[65%_35%] mt-5 h-[400px] gap-6">
+      <div className="grid grid-cols-[65%_35%] mt-5 h-[250px] gap-6 pr-4">
         <div className="w-full h-full">
           <h1 className="text-xl font-bold mb-4">Market Overview</h1>
-          <MarketOverviewNoSSR
-            showChart={true}
-            height={320}
-            width={660}
-            plotLineColorGrowing="#46B49E"
-            plotLineColorFalling="#46B49E"
-            belowLineFillColorFalling="rgba(70, 180, 158, 0)"
-            belowLineFillColorGrowing="rgba(70, 180, 158, 0.12)"
-            belowLineFillColorFallingBottom="rgba(70, 190, 158, 1)"
-            belowLineFillColorGrowingBottom="rgba(70, 190, 158, 0.08)"
-          />
+            <MarketOverviewNoSSR
+              showChart={true}
+              height={305}
+              width={660}
+              plotLineColorGrowing="#46B49E"
+              plotLineColorFalling="#46B49E"
+              belowLineFillColorFalling="rgba(70, 180, 158, 0)"
+              belowLineFillColorGrowing="rgba(70, 180, 158, 0.12)"
+              belowLineFillColorFallingBottom="rgba(70, 190, 158, 1)"
+              belowLineFillColorGrowingBottom="rgba(70, 190, 158, 0.08)"
+            />
         </div>
         <div className="bg-white p-4 rounded-2xl">
           <h1 className="text-xl font-bold">My Watchlist</h1>
